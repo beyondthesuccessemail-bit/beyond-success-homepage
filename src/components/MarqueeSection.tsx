babf4646ed714e75aa13efc1584cc72b9ed1911c
motiongraphicdesigncompany-code <motiongraphicdesigncompany@gmail.com>
2026-04-06 22:00:37 +0200

    added to slider menu

--- src/components/MarqueeSection.tsx
+++ src/components/MarqueeSection.tsx
@@ -1,6 +1,6 @@
 const words = [
   "SOCIAL MEDIA", "•", "CONTENT", "•", "EMAIL MARKETING", "•", "VIDEOGRAPHY", "•",
-  "PPC", "•", "SEO", "•", "DIGITAL", "•", "SOCIAL", "•", "PAID SOCIAL", "•",
+  "PPC", "•", "SEO", "•", "DIGITAL", "•", "SOCIAL", "•", "PAID SOCIAL", "•", "WEBSITE DESIGN", "•",
 ];
 
 const MarqueeSection = () => {
